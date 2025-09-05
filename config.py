bfa274a2ae286ab084a1dfef4f46bd7fd0e23b70
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-09-05 21:38:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,22 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "29907731"))
+API_ID = int(os.environ.get("API_ID", "21157244"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "8f59d632cb374705cfdee46ac17cc3cd")
+API_HASH = os.environ.get("API_HASH", "4981c2699bd91c7db836ec8f77e5b0f0")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7591431005:AAHSTGbhrKqbddHPpkauBl7eJwh68nn544k")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8347630166:AAFCrvaixyRfrrz_dLreB5mJdVDgbQzDlDc")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("@mjteamexbot")
+BOT_USERNAME = os.environ.get("@testseriesgajju_bot")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "6697397532"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1783306092"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
 # ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002475668850"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002887045646"))
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://mehuldb:ymLdv8Sf2UW49x2m@withcluster.akoofaz.mongodb.net/")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://saraswatisharmapbc:L8aRiWhBbhi8mFwN@cluster0.gxhxgye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 # -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002617386015"))
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002887045646"))
 
